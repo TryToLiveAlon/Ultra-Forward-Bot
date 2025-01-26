433a92f18258b652952ac01b5dbabbe708d668c8
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-01-26 11:35:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,9 +13,9 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "700092e37d7da9a7b781994b7503a488")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6140468904').split()]
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://ayushop:ayushop210@cluster0.gklnc.mongodb.net/")
+    DB_NAME = os.environ.get("DB_NAME", "Cluster0")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '6660736046').split()]
 
 
 class temp(object): 
